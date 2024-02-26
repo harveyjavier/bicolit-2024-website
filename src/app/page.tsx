808f1bf9ca66c80f440e5dcad1072771c8e97bd8
Peter John Arao <pitzzahh@gmail.com>
2024-02-26 23:44:58 +0800

    💄 Add gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import About from "@/components/about";
 import Hero from "@/components/hero";
+import Header from "@/components/header";
 
 export default function Home() {
   return (
     <>
       <main>
-        <Hero />
+        <section className="relative flex items-start justify-between flex-col h-svh md:h-screen bg-gradient-to-tl to-[#00C2FF] via-[45.814%] via-[#6633C2] from-[#330066] pb-5 opacity-[99%]">
+          <Header />
+          <Hero />
+        </section>
         <About />
       </main>
     </>
