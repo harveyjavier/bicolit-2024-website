bd7a5811fc204aa7be59af9831e4ccfbc00ba3b1
Peter John Arao <pitzzahh@gmail.com>
2024-02-26 16:59:00 +0800

    🔧 Add site configuration

---
+++ src/config/site.ts
@@ -0,0 +1,35 @@
+export const siteConfig = {
+  name: "Bicol IT",
+  url: "https://bicolit.org",
+  ogImage: "https://bicolit.org/images/bicolit-logo.png",
+  description:
+    "Transforming Bicol into a globally competitive Information Technology hub through Education and Technopreneurship.",
+  links: [
+    {
+      title: "About Bicol IT",
+      href: "#about",
+    },
+    {
+      title: "Events",
+      href: "#events",
+    },
+    {
+      title: "Membership",
+      href: "#membership",
+    },
+    {
+      title: "Blogs",
+      href: "#blogs",
+    },
+    {
+      title: "Advocates",
+      href: "#advocates",
+    },
+    {
+      title: "Partners",
+      href: "#partners",
+    },
+  ],
+};
+
+export type SiteConfig = typeof siteConfig;
