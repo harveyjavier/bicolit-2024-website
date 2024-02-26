d21eccfd7cf0cd3e4f406d144397c825e05bd070
Peter John Arao <pitzzahh@gmail.com>
2024-02-26 23:46:05 +0800

    ✨ Add hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,18 +2,28 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative container">
-      <span className="text-8xl text-wrap font-bold  top-1/2 left-10 transform -translate-y-1/2 z-14 w-1">
-        Innovating Tomorrow, Today.
-      </span>
-      {/* <Image
-        className="absolute top-0 h-[96] left-0"
-        src="/assets/hero.jpg"
-        alt="Hero Image"
-        width={1920}
-        height={1080}
+    <>
+      <Image
+        className="absolute top-0 right-0 h-fit"
+        src="/assets/graphic_1.png"
+        alt="Nice Graphic"
+        height={500}
+        width={500}
         priority
-      /> */}
-    </section>
+      />
+      <Image
+        className="absolute right-0 md:right-14 bottom-0 h-min w-50"
+        src="/assets/vr-girl.png"
+        alt="A Girl in a VR Headset"
+        height={600}
+        width={600}
+        priority
+      />
+      <span className="text-white pl-6 grow font-helvetica_bold text-5xl md:text-7xl lg:text-9xl md:pl-24 pt-6 md:pt-20 text-wrap font-bold transform">
+        Innovating
+        <br /> Tomorrow,
+        <br /> Today.
+      </span>
+    </>
   );
 }
