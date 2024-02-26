f0e5ffe073542c6699ebcd93545ad28bc0d223e6
Peter John Arao <pitzzahh@gmail.com>
2024-02-26 16:52:28 +0800

    💄 Add about component (not yet done)

---
+++ src/components/about.tsx
@@ -0,0 +1,36 @@
+import Hero from "./hero";
+
+export default function About() {
+  return (
+    <section
+      className="flex items-start z-14 justify-center px-36 pt-14 gap-10 my-10 pb-36 bg-white dark:text-black"
+      id="about"
+    >
+      <h2 dir="rtl" className="font-bold text-xl">
+        We're the biggest and most active IT education advocacy organization in
+        the Bicol region with 5k+ members
+      </h2>
+      <div className="flex item-center text-sm flex-col justify-center gap-4 flex-wrap">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </p>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+      </div>
+    </section>
+  );
+}
