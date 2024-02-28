648def4ada83f3da48c8bf0cbce9451583359d33
Peter John Arao <pitzzahh@gmail.com>
2024-02-28 11:09:18 +0800

    🔧 Add logo, and keywords site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,11 +1,14 @@
 import logo from "../../public/assets/logo.svg";
+import ogImage from "../../public/assets/og.png";
 
 export const siteConfig = {
   name: "Bicol IT",
   url: "https://bicolit.org",
-  ogImage: logo,
+  logo,
+  ogImage,
   description:
     "Transforming Bicol into a globally competitive Information Technology hub through Education and Technopreneurship.",
+  keywords: ["bicolit", "Bicol IT", "Bicol Information Technology", "Bicolit"],
   links: [
     {
       title: "About Bicol IT",
