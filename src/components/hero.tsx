c65bf171123db249e437d7da1c60210d075f656f
Peter John Arao <pitzzahh@gmail.com>
2024-02-28 10:46:01 +0800

    💄 Refactor hero section to use flex-box

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,19 +11,21 @@ export default function Hero() {
         width={500}
         priority
       />
-      <Image
-        className="absolute right-0 md:right-14 bottom-0 h-min w-50"
-        src="/assets/vr-girl.png"
-        alt="A Girl in a VR Headset"
-        height={600}
-        width={600}
-        priority
-      />
-      <span className="text-white pl-6 grow font-helvetica_bold text-5xl md:text-7xl lg:text-9xl md:pl-24 pt-6 md:pt-20 text-wrap font-bold transform">
-        Innovating
-        <br /> Tomorrow,
-        <br /> Today.
-      </span>
+      <div className="absolute bottom-0 flex z-10 md:px-20 items-start justify-start md:items-end md:justify-center flex-col-reverse md:flex-row-reverse">
+        <Image
+          className="h-min w-50 align-bottom"
+          src="/assets/vr-girl.png"
+          alt="A Girl in a VR Headset"
+          height={620}
+          width={620}
+          priority
+        />
+        <div className="text-white md:translate-x-10 md:pb-20 font-helvetica_bold text-5xl md:text-6xl lg:text-9xl font-bold">
+          <span className="block">Innovating</span>
+          <span className="block">Tomorrow,</span>
+          <span className="block">Today.</span>
+        </div>
+      </div>
     </>
   );
 }
