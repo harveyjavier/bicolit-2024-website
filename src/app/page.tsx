c71d9196c37dc154ae20e794117575338868d8f4
Peter John Arao <pitzzahh@gmail.com>
2024-02-28 10:44:47 +0800

    🐛 Fix main section overlapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
-import About from "@/components/about";
-import Hero from "@/components/hero";
 import Header from "@/components/header";
+import Hero from "@/components/hero";
+import About from "@/components/about";
+import Events from "@/components/events";
+import Membership from "@/components/membership";
 
 export default function Home() {
   return (
-    <>
-      <main>
-        <section className="relative flex items-start justify-between flex-col h-svh md:h-screen bg-gradient-to-tl to-[#00C2FF] via-[45.814%] via-[#6633C2] from-[#330066] pb-5 opacity-[99%]">
-          <Header />
-          <Hero />
-        </section>
-        <About />
-      </main>
-    </>
+    <main>
+      <section className="container relative flex items-start justify-between flex-col h-svh md:h-screen bg-gradient-to-tl to-[#00C2FF] via-[45.814%] via-[#6633C2] from-[#330066] pb-5">
+        <Header />
+        <Hero />
+      </section>
+      <About />
+      <Events />
+      <Membership />
+    </main>
   );
 }
