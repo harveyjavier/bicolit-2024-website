9bf8546d81eef2bf354e03075c10c533c8d54a04
Peter John Arao <pitzzahh@gmail.com>
2024-02-29 22:46:36 +0800

    🔧 Add form details and contacts

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,6 +6,9 @@ import three from "../../public/assets/advocates/3.png";
 import four from "../../public/assets/advocates/4.png";
 import five from "../../public/assets/advocates/5.png";
 import six from "../../public/assets/advocates/6.png";
+import facebook from "../../public/assets/partners/facebook.svg";
+import instagram from "../../public/assets/partners/ig.svg";
+import x from "../../public/assets/partners/x.svg";
 
 export const siteConfig = {
   name: "Bicol IT",
@@ -79,6 +82,62 @@ export const siteConfig = {
       position: "Student Council President",
     },
   ],
+  formDetails: [
+    {
+      placeholder: "First Name",
+      type: "text",
+      name: "first_name",
+    },
+    {
+      placeholder: "Last Name",
+      type: "text",
+      name: "last_name",
+    },
+    {
+      placeholder: "Email Address",
+      type: "email",
+      name: "email",
+    },
+    {
+      placeholder: "Mobile Number",
+      type: "tel",
+      name: "mobile_number",
+    },
+  ],
+  contacts: {
+    info: [
+      {
+        key: "HQ",
+        value:
+          "2F RJV Commercial Building, Rizal St., Cor. Gov. Reynolds St., Old Albay District, Legazpi City, 4500, Albay, Philippines",
+      },
+      {
+        key: "Mobile",
+        value: "+63 9999123456",
+      },
+      {
+        key: "Email",
+        value: "hello@bicolit.org",
+      },
+    ],
+    links: [
+      {
+        name: "Facebook",
+        logo: facebook,
+        url: "https://www.facebook.com/bicolit.org",
+      },
+      {
+        name: "Instagram",
+        logo: instagram,
+        url: "https://instagram.com/bicolit",
+      },
+      {
+        name: "X",
+        logo: x,
+        url: "https://x.com/bicolit",
+      },
+    ],
+  },
 };
 
 export type SiteConfig = typeof siteConfig;
