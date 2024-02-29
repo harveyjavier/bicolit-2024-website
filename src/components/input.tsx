200e02d9a9f42694c4b1b19c5184da17647fefc5
Peter John Arao <pitzzahh@gmail.com>
2024-02-29 19:06:02 +0800

    ♻️ Refactor input component (💩code)

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -3,13 +3,27 @@ import React from "react";
 type InputProps = {
   placeholder: string;
   type?: string;
+  className?: string;
+  required?: boolean;
+  pattern?: string;
+  name: string;
 };
 
-const Input: React.FC<InputProps> = ({ placeholder, type }) => {
+const Input: React.FC<InputProps> = ({
+  placeholder,
+  type,
+  name,
+  className,
+  required,
+  pattern,
+}) => {
   return (
     <input
-      className="w-full stroke-[#ABA1B6]"
+      className={`w-full border-[#ABA1B6] focus:border-[#ABA1B6] rounded-md px-4 py-2 bg-[#E5DFEB] ${className}`}
       type={type}
+      name={name}
+      pattern={pattern}
+      required={required}
       placeholder={placeholder}
     />
   );
