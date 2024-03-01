ea91424766628d03bcff79538170c88ad9b72539
Peter John Arao <pitzzahh@gmail.com>
2024-03-01 14:05:47 +0800

    💄 Update email ui (💩wip)

--- src/components/email/index.tsx
+++ src/components/email/index.tsx
@@ -1,24 +1,35 @@
 import * as React from "react";
-import { Hr, Html, Heading, Img } from "@react-email/components";
+import { Hr, Html, Heading, Img, Tailwind } from "@react-email/components";
 import { siteConfig } from "@/config/site";
 
 export function Email(props: EmailProps) {
-  const { name, subject, content } = props;
-
   return (
-    <Html lang="en" className="flex items-center justify-center flex-col">
-      <Img
-        src="/bicolit-logo.png"
-        alt="Bicol IT Logo"
-        width={166}
-        height={166}
-      />
-      <Hr />
-      <Heading as="h1">{subject}</Heading>
-      <div>
-        <span>Name: {name}</span>
-      </div>
-    </Html>
+    <Tailwind>
+      <Html
+        lang="en"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          background:
+            "linear-gradient(to top left, #00C2FF 0%, #6633C2 60%, #330066 100%)",
+        }}
+      >
+        <Img
+          src={`${siteConfig.url}/images/bicolit-logo.png`}
+          alt="Bicol IT Logo"
+          width={166}
+          height={166}
+          style={{ margin: "0 auto" }}
+        />
+        <Hr />
+        <Heading as="h1">{props.subject}</Heading>
+        <div>
+          <span>Name: {props.name}</span>
+        </div>
+      </Html>
+    </Tailwind>
   );
 }
 
