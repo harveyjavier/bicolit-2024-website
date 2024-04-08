5500b0feb8e16a7847e77eac0f2b02d9b60cfcf8
Peter John Arao <pitzzahh@gmail.com>
2024-04-08 21:28:11 +0800

    ✨ Add founders sections

---
+++ src/components/founder.tsx
@@ -0,0 +1,32 @@
+import Card from "@/components/card";
+import { siteConfig } from "@/config/site";
+import AdvocateTitle from "./advocate-title";
+import linkedinIcon from "../../public/assets/linkedin.svg";
+import SectionHeader from "./section-header";
+
+export default function Founder() {
+  return (
+    <div className="container px-4 py-6 md:px-24 flex h-auto flex-col justify-around gap-6">
+      <SectionHeader id="founder" title="The Founder" />
+      <div className="flex flex-wrap items-center justify-center lg:justify-between gap-4">
+        {siteConfig.founders.map((founder, index) => (
+          <Card
+            cardClass="even:my-4 hover:cursor-pointer"
+            imageSource={founder.imageSource}
+            imageWidth={333}
+            key={index}
+            imageHeight={333}
+            title={
+              <AdvocateTitle
+                title={founder.name}
+                url={founder.linkedin}
+                icon={linkedinIcon}
+              />
+            }
+            description={founder.position}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
