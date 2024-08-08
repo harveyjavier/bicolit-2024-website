2c0ab024333d6bdeeb9bacf4e0512e0ab66ae5c6
Peter John Arao <pitzzahh@gmail.com>
2024-08-08 19:53:15 +0800

    ♻️ Refactor student council image file extensions and add STI, SLTCFI logo

--- src/assets/student-council/school-logo/index.ts
+++ src/assets/student-council/school-logo/index.ts
@@ -2,4 +2,5 @@ export { default as ADNULogo } from "./adnu-logo.png";
 export { default as BULogo } from "./bu-logo.png";
 export { default as CSPCLogo } from "./cspc-logo.png";
 export { default as DWCLLogo } from "./dwcl-logo.png";
-export { default as STICLogo } from "./stic-logo.png";
+export { default as STICLogo } from "./sti-logo.png";
+export { default as SLTCFILogo } from "./sltcfi-logo.jpeg";
